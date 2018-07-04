ba955f43aa0dc1b7bd653529f694dee4d5995a15
Israelistic <haguyle@gmail.com>
2018-07-04 17:43:53 -0400

    added routes to edit,show,new. added views for users edit, show. added placeholder values to form. added methods edit, update, load_user, show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,18 +15,36 @@ before_action :load_users, only:[:show, :edit, :update, :destory]
       session[:user_id] = @user.id
       redirect_to projects_url
     else
-      render 'new'
+      render :new
     end
   end
 
   def show
-
   end
+
   def edit
   end
   def update
+    @user.first_name = params[:user][:first_name]
+    @user.last_name = params[:user][:last_name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.save
+
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+
   end
+
   def load_users
     @user = User.find(params[:id])
   end
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :pledges, only: [:create]
     resources :rewards, only: [:new, :create, :destroy]
   end
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :user_sessions, only: [:create]
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  # Example of regular route:
+  # Example of regular route:s
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
